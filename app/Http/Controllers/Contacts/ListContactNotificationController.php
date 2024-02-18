c2c5abdef27b9dcdebca676ac3654f99852ecb1a
cardial <luccascardial@gmail.com>
2024-02-18 20:58:23 -0300

    feat(http): list contact notifications

---
+++ app/Http/Controllers/Contacts/ListContactNotificationController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Contacts;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\Request;
+use Modules\Contact\Enums\ContactNotificationStatusEnum;
+use Modules\Contact\Queries\ListContactNotification\ListContactNotificationQuery;
+use Modules\Contact\Queries\ListContactNotification\ListContactNotificationQueryHandler;
+
+class ListContactNotificationController extends Controller
+{
+    public function __construct(
+        private readonly ListContactNotificationQueryHandler $queryHandler
+    )
+    {
+    }
+
+    public function __invoke(Request $request): LengthAwarePaginator
+    {
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 5);
+
+        $status = ContactNotificationStatusEnum::PENDING;
+
+        return $this->queryHandler->handle(new ListContactNotificationQuery(
+            page: $page, limit: $limit, status: $status));
+    }
+}
