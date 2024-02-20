60698ac08b0c1050f0fa50dbc7e76441b179c1be
cardial <luccascardial@gmail.com>
2024-02-20 06:23:28 -0300

    style: grammar

--- src/Modules/Contact/Resource/lang/pt-br/errors.php
+++ src/Modules/Contact/Resource/lang/pt-br/errors.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'invalid_sheets_template' => [
-        'title' => 'Palinha inválida',
+        'title' => 'Planihla inválida',
         'detail' => 'O template da planilha é enviado inválido',
         'helper_text' => 'Você pode baixar um exemplo de template clicando no link abaixo:'
     ],
